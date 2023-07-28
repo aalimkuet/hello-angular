0f76f79543d3efc1c5fdc779bffa245410cc2394
Abdul Alim <abdul.alim@.brainstation-23.com>
2023-07-28 16:44:49 +0600

    folder orginization

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './components/hello/hello.component';
+import { HelloComponent } from '../hello/hello.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from '../home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
